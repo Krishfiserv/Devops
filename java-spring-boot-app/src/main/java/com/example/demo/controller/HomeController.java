a4853018faec60fd9f8d5c1c1af937dce5b4eed8
rkannagala <93792117+rkannagala@users.noreply.github.com>
2021-11-30 10:28:46 -0600

    Update HomeController.java

--- java-spring-boot-app/src/main/java/com/example/demo/controller/HomeController.java
+++ java-spring-boot-app/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,1 +1,13 @@
-hi
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Azure App Service (in the staging slot)!";
+    }
+}
